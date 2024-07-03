a7b89152bcaebf65bfadfd874792051a678bc3c6
Neiel Jair <neiel@hellopearl.com>
2024-07-02 21:44:41 -0300

    fix: now all label related things use arbitrary colors

--- client/src/components/Label/Label.jsx
+++ client/src/components/Label/Label.jsx
@@ -12,40 +12,12 @@ const SIZES = {
   MEDIUM: 'medium',
 };
 
-const LEGACY_COLORS = {
-  'berry-red': '#e04556',
-  'pumpkin-orange': '#f0982d',
-  'lagoon-blue': '#109dc0',
-  'pink-tulip': '#f97394',
-  'light-mud': '#c7a57b',
-  'orange-peel': '#fab623',
-  'bright-moss': '#a5c261',
-  'antique-blue': '#6c99bb',
-  'dark-granite': '#8b8680',
-  'lagune-blue': '#00b4b1',
-  'sunny-grass': '#bfca02',
-  'morning-sky': '#52bad5',
-  'light-orange': '#ffc66d',
-  'midnight-blue': '#004d73',
-  'tank-green': '#8aa177',
-  'gun-metal': '#355263',
-  'wet-moss': '#4a8753',
-  'red-burgundy': '#ad5f7d',
-  'light-concrete': '#afb0a4',
-  'apricot-red': '#fc736d',
-  'desert-sand': '#edcb76',
-  'navy-blue': '#166a8f',
-  'egg-yellow': '#f7d036',
-  'coral-green': '#2b6a6c',
-  'light-cocoa': '#87564a',
-}; // TODO delete
-
 const Label = React.memo(({ name, color, size, isDisabled, onClick }) => {
   const contentNode = (
     <div
       title={name}
       style={{
-        '--background': color.includes('#') ? color : LEGACY_COLORS[color],
+        '--background': color,
       }}
       className={classNames(
         styles.wrapper,
